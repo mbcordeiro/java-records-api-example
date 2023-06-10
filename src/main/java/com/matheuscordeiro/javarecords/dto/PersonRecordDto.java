8bfd78a7d3375d154bafa30501ff3e3064fd346f
Matheus Cordeiro <55818513+mbcordeiro@users.noreply.github.com>
2023-06-09 23:58:47 -0300

    refactor

--- src/main/java/com/matheuscordeiro/javarecords/dto/PersonRecordDto.java
+++ src/main/java/com/matheuscordeiro/javarecords/dto/PersonRecordDto.java
@@ -4,7 +4,8 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-public record PersonRecordDto(@NotNull @Size(max = 100) String name, @NotNull @Size(min = 2) int age) {
+public record PersonRecordDto(@NotNull @Size(max = 100) String name, 
+    @NotNull @Size(min = 2) int age) {
     public static final String MESSAGE = "Hello World";
 
     public void printName() {
